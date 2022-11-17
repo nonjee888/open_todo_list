74dfed07b001d591ebcf6ff1f372bc5a3e68bbae
Eunji Roh <nonjee888@gmail.com>
2022-11-17 20:24:11 +0900

    [refactor] list에서 map 실행

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,23 @@
 import styled from "styled-components";
 import Todo from "./Todo";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTodos } from "../redux/modules/todos";
 
 const List = (props) => {
+  const dispatch = useDispatch();
+  const { todos } = useSelector((state) => state.todos);
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, []);
+
   return (
     <StListDiv>
       <StTodosDiv>
-        <Todo props={props} />
+        {todos.map((todo) => {
+          return <Todo props={props} todo={todo} key={todo.id} />;
+        })}
       </StTodosDiv>
     </StListDiv>
   );
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,20 +1,9 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchTodos } from "../redux/modules/todos";
 
-const Todo = ({ props }) => {
+const Todo = ({ props, todo }) => {
   const { checkedItems, setCheckedItems } = props;
-  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { todos } = useSelector((state) => state.todos);
-
-  useEffect(() => {
-    dispatch(fetchTodos());
-  }, []);
-
-  // 체크 될 아이템 담을 배열
 
   // 체크박스 선택
   const handleSingleCheck = (checked, id) => {
@@ -29,7 +18,7 @@ const Todo = ({ props }) => {
   const handleAllCheck = (checked) => {
     if (checked) {
       const idArray = [];
-      todos.forEach((el) => idArray.push(el.id));
+      todo.forEach((el) => idArray.push(el.id));
       setCheckedItems(idArray);
     } else {
       setCheckedItems([]);
@@ -39,31 +28,27 @@ const Todo = ({ props }) => {
   return (
     <>
       <StTodoUl id="sTable">
-        {todos.map((todo) => {
-          return (
-            <StTodoLi key={todo.id}>
-              <StInput
-                id={todo.id}
-                type="checkbox"
-                name="todoChecked"
-                onChange={(e) => {
-                  handleSingleCheck(e.target.checked, todo.id);
-                }}
-                checked={checkedItems.includes(todo.id) ? true : false}
-              />
+        <StTodoLi>
+          <StInput
+            id={todo.id}
+            type="checkbox"
+            name="todoChecked"
+            onChange={(e) => {
+              handleSingleCheck(e.target.checked, todo.id);
+            }}
+            checked={checkedItems.includes(todo.id) ? true : false}
+          />
 
-              <StTextdiv
-                onClick={() => {
-                  navigate("/" + todo.id);
-                }}
-              >
-                {todo.text}
-              </StTextdiv>
+          <StTextdiv
+            onClick={() => {
+              navigate("/" + todo.id);
+            }}
+          >
+            {todo.text}
+          </StTextdiv>
 
-              <div>{todo.deadLine}</div>
-            </StTodoLi>
-          );
-        })}
+          <div>{todo.deadLine}</div>
+        </StTodoLi>
       </StTodoUl>
     </>
   );
