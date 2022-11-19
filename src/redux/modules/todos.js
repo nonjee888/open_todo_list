8441bbb1b3e052df4e9326c2f8c0eb9c24b9b12e
Eunji Roh <nonjee888@gmail.com>
2022-11-19 21:49:49 +0900

    [refactor] 주석제거

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -21,7 +21,6 @@ export const getTodos = createAsyncThunk(
       const { data } = await axios.get(
         process.env.REACT_APP_HOST + `/api/todos/${payload}`
       );
-      console.log(data);
       return thunkAPI.fulfillWithValue(data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
